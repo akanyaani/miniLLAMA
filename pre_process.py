b6191acf1f0cfa1d5e0b9cabbf4b111d2a165cb5
Abhay Kumar <akanyaani@gmail.com>
2024-02-17 14:30:10 +0530

    tokenization issue fixed

--- pre_process.py
+++ pre_process.py
@@ -3,36 +3,43 @@ import click
 import sentencepiece as spm
 from datasets import load_dataset
 
-
 num_proc_load_dataset = 4
 # Just downloading 5% of data for faster process
-dataset = load_dataset("openwebtext", split="train[:5%]", num_proc=num_proc_load_dataset)
+dataset = load_dataset("openwebtext", streaming=True)
+
+## Downloading Sample Data from openwebtext, Can be used large corpus as well for my be language and custome models
+sample_data = []
+for i, row in enumerate(dataset["train"]):
+    sample_data.append(row["text"])
+    if i > 5000:
+        break
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 PROCESS_DATA_TXT = _ROOT + "/data/processed.txt"
 BPE_MODEL_PATH = _ROOT + "/model/tokenizer.model"
-PROCESS_DATA_PATH = _ROOT + "/data/processed_data"
 
 
 def process_text():
     print("Pre-processing the text data.....")
+    folder_path = os.path.dirname(PROCESS_DATA_TXT)  # Extract the parent directory
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
     with open(PROCESS_DATA_TXT, "w", encoding="utf-8") as file:
         # Write training data
-        for row in dataset["train"]:
-            file.write(row["text"] + "\n")
-        # Write testing data
-        for row in dataset["val"]:
-            file.write(row["text"] + "\n")
-    dataset.save_to_disk(PROCESS_DATA_PATH)
+        for text in sample_data:
+            file.write(text + "\n")
 
 
 def train_byte_pair_encoding(vocab_size):
     print("Training BytePair encoding......")
+    folder_path = os.path.dirname(BPE_MODEL_PATH)  # Extract the parent directory
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
 
     spmcmd = ('--input={spm_input} --model_prefix={spm_model} --vocab_size={vocab_size} '
               '--user_defined_symbols=<s>,</s> --hard_vocab_limit=false --model_type=bpe --pad_id=0 --unk_id=1 '
               '--bos_id=-1 --eos_id=-1 --pad_piece=[PAD] --unk_piece=[UNK]').format(
-        spm_input=PROCESS_DATA_PATH, spm_model=BPE_MODEL_PATH, vocab_size=vocab_size)
+        spm_input=PROCESS_DATA_TXT, spm_model=BPE_MODEL_PATH, vocab_size=vocab_size)
     spm.SentencePieceTrainer.train(spmcmd)
 
 
